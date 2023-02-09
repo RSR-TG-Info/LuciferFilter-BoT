f7d5188641095e559f3ecbc6768c92ff479edb81
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-09 22:26:39 +0530

    Update x-WORKER-x.py

--- LuciferFilter/modules/x-WORKER-x.py
+++ LuciferFilter/modules/x-WORKER-x.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
-from Midukki.midukki import Midukki_RoboT
-from Midukki.modules.auto_filters import auto_filters, send_for_index
-from Midukki.modules.manual_filters import manual_filters
-from Midukki.functions.settings import get_settings
-from Midukki.database import db
+from LuciferFilter.luciferfilter import LuciferFilter-BoT
+from LuciferFilter.modules.auto_filters import auto_filters, send_for_index
+from LuciferFilter.modules.manual_filters import manual_filters
+from LuciferFilter.functions.settings import get_settings
+from LuciferFilter.database import db
 
 
-@Midukki_RoboT.on_message(filters.group & filters.text & filters.incoming)
+@LuciferFilter-BoT.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
 
     k = await manual_filters(client, message)
@@ -15,6 +15,6 @@ async def give_filter(client, message):
         if settings["autofilter"]:
             await auto_filters(client, message)
 
-@Midukki_RoboT.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
+@LuciferFilter-BoT.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
 async def start_for_index(client, message):
     await send_for_index(client, message)
