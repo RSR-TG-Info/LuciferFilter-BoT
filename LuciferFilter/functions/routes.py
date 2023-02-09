64f1ef0226416db1d5728728f3e6563bfcac4df9
BIKASH <124684607+Pr0fess0r99@users.noreply.github.com>
2023-02-09 23:22:23 +0530

    Update routes.py

--- LuciferFilter/functions/routes.py
+++ LuciferFilter/functions/routes.py
@@ -1,8 +1,8 @@
 from aiohttp import web
-from Midukki import Configs
+from LuciferFilter import Configs
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=Configs.STOP_BOT)
 async def root_route_handler(request):
-    return web.json_response("Mo_Tech_YT")
+    return web.json_response("TechProjectsUpdates")
